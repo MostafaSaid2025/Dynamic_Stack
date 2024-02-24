91cead58ff33865f36ff0944ed5f7f638c1d2512
MostafaSaid2025 <153297224+MostafaSaid2025@users.noreply.github.com>
2024-02-24 15:43:01 +0200

    Update Dynamin_Stack.c

--- Dynamin_Stack.c
+++ Dynamin_Stack.c
@@ -5,10 +5,28 @@
 stack_status_t stack_full(stack_ds_t *my_stack);
 stack_status_t stack_empty(stack_ds_t *my_stack);
 
-
-stack_ds_t *create_stack  (unsigned int max_size, stack_status_t *ret_stack)
+/**
+ * @brief Creates a new stack with the specified maximum size.
+ * 
+ * This function dynamically allocates memory for a new stack structure 
+ * and initializes its attributes. The maximum size of the stack is 
+ * specified by the parameter max_size. The status of the operation 
+ * is returned through the parameter ret_stack.
+ * 
+ * @param max_size The maximum size of the stack.
+ * @param ret_stack Pointer to a stack_status_t where the status 
+ *                  of the operation will be stored.
+ *                  Possible values are:
+ *                  - STACK_OK: Operation succeeded.
+ *                  - STACK_NULL_POINTER: Invalid pointer.
+ * 
+ * @return Pointer to the newly created stack structure if successful, 
+ *         otherwise NULL.
+ */
+stack_ds_t *create_stack(unsigned int max_size, stack_status_t *ret_stack)
 {
     stack_ds_t *my_stack = NULL;
+
     if (NULL == ret_stack)
     {
         *ret_stack = STACK_NULL_POINTER;
@@ -35,17 +53,28 @@ stack_ds_t *create_stack  (unsigned int max_size, stack_status_t *ret_stack)
             }
             else
             {
-                #ifdef stack_DEBUG
-                printf("Stack has been created\n");
-                #endif // stack_DEBUG
                 *ret_stack = STACK_OK;
             }
         }
-
     }
     return my_stack;
 }
-stack_ds_t *destroy_stack (stack_ds_t *my_stack, stack_status_t *ret_stack)
+
+/**
+ * @brief Destroys the stack and releases allocated memory.
+ * 
+ * This function deallocates memory associated with the specified stack 
+ * and sets the stack pointer to NULL. The status of the operation 
+ * is returned through the parameter ret_stack.
+ * 
+ * @param my_stack Pointer to the stack structure.
+ * @param ret_stack Pointer to a stack_status_t where the status 
+ *                  of the operation will be stored.
+ *                  Possible values are:
+ *                  - STACK_OK: Operation succeeded.
+ *                  - STACK_NULL_POINTER: Invalid pointer.
+ */
+stack_ds_t *destroy_stack(stack_ds_t *my_stack, stack_status_t *ret_stack)
 {
     if (NULL == my_stack || NULL == ret_stack)
     {
@@ -56,15 +85,26 @@ stack_ds_t *destroy_stack (stack_ds_t *my_stack, stack_status_t *ret_stack)
         free(my_stack->stack_array);
         free(my_stack);
         *ret_stack = STACK_OK;
-        #ifdef stack_DEBUG
-        printf("Stack has been destroyed\n");
-        #endif // stack_DEBUG
     }
     return NULL;
 }
-stack_status_t push_stack (stack_ds_t *my_stack, void *element)
+
+/**
+ * @brief Inserts an element onto the top of the stack.
+ * 
+ * This function inserts the specified element onto the top of the stack.
+ * If the stack is full, the operation fails, and the appropriate status 
+ * code is returned.
+ * 
+ * @param my_stack Pointer to the stack structure.
+ * @param element Pointer to the element to be inserted.
+ * 
+ * @return stack_status_t indicating the status of the operation.
+ */
+stack_status_t push_stack(stack_ds_t *my_stack, void *element)
 {
     stack_status_t ret = STACK_NOT_OK;
+
     if (NULL == my_stack || NULL == element)
     {
         ret = STACK_NULL_POINTER;
@@ -81,19 +121,34 @@ stack_status_t push_stack (stack_ds_t *my_stack, void *element)
             my_stack->stack_array[my_stack->stack_top] = element;
             my_stack->stack_count++;
             ret = STACK_OK;
-            #ifdef stack_DEBUG
-            printf("Pushing done successfully\n");
-            #endif // stack_DEBUG
         }
-
     }
     return ret;
-
 }
-void *pop_stack(stack_ds_t *my_stack , stack_status_t *ret_stack)
+
+/**
+ * @brief Removes and returns the element at the top of the stack.
+ * 
+ * This function removes and returns the element at the top of the stack.
+ * If the stack is empty, the operation fails, and the appropriate status 
+ * code is returned.
+ * 
+ * @param my_stack Pointer to the stack structure.
+ * @param ret_stack Pointer to a stack_status_t where the status 
+ *                  of the operation will be stored.
+ *                  Possible values are:
+ *                  - STACK_OK: Operation succeeded.
+ *                  - STACK_NULL_POINTER: Invalid pointer.
+ *                  - STACK_EMPTY: Stack is empty.
+ * 
+ * @return Pointer to the removed element if successful, 
+ *         otherwise NULL.
+ */
+void *pop_stack(stack_ds_t *my_stack, stack_status_t *ret_stack)
 {
     void *ptr = NULL;
     *ret_stack = STACK_NOT_OK;
+
     if (NULL == my_stack || NULL == ret_stack)
     {
         *ret_stack = STACK_NULL_POINTER;
@@ -106,21 +161,38 @@ void *pop_stack(stack_ds_t *my_stack , stack_status_t *ret_stack)
         }
         else
         {
-            ptr =  my_stack->stack_array[my_stack->stack_top];
+            ptr = my_stack->stack_array[my_stack->stack_top];
             my_stack->stack_top--;
             my_stack->stack_count--;
             *ret_stack = STACK_OK;
-            #ifdef stack_DEBUG
-            printf("Poping done successfully\n");
-            #endif // stack_DEBUG
         }
     }
-    return ptr ;
+    return ptr;
 }
-void *stack_top  (stack_ds_t *my_stack, stack_status_t *ret_stack)
+
+/**
+ * @brief Returns the element at the top of the stack without removing it.
+ * 
+ * This function returns the element at the top of the stack without removing it.
+ * If the stack is empty, the operation fails, and the appropriate status 
+ * code is returned.
+ * 
+ * @param my_stack Pointer to the stack structure.
+ * @param ret_stack Pointer to a stack_status_t where the status 
+ *                  of the operation will be stored.
+ *                  Possible values are:
+ *                  - STACK_OK: Operation succeeded.
+ *                  - STACK_NULL_POINTER: Invalid pointer.
+ *                  - STACK_EMPTY: Stack is empty.
+ * 
+ * @return Pointer to the element at the top of the stack if successful, 
+ *         otherwise NULL.
+ */
+void *stack_top(stack_ds_t *my_stack, stack_status_t *ret_stack)
 {
-   void *ptr = NULL;
+    void *ptr = NULL;
     *ret_stack = STACK_NOT_OK;
+
     if (NULL == my_stack || NULL == ret_stack)
     {
         *ret_stack = STACK_NULL_POINTER;
@@ -133,18 +205,28 @@ void *stack_top  (stack_ds_t *my_stack, stack_status_t *ret_stack)
         }
         else
         {
-            ptr =  my_stack->stack_array[my_stack->stack_top];
+            ptr = my_stack->stack_array[my_stack->stack_top];
             *ret_stack = STACK_OK;
-            #ifdef stack_DEBUG
-            printf("top done successfully\n");
-            #endif // stack_DEBUG
         }
     }
-    return ptr ;
+    return ptr;
 }
-stack_status_t stack_count  (stack_ds_t *my_stack, unsigned int *stack_count)
+
+/**
+ * @brief Returns the current count of elements in the stack.
+ * 
+ * This function returns the current count of elements in the stack.
+ * 
+ * @param my_stack Pointer to the stack structure.
+ * @param stack_count Pointer to an unsigned integer where the count 
+ *                    of elements will be stored.
+ * 
+ * @return stack_status_t indicating the status of the operation.
+ */
+stack_status_t stack_count(stack_ds_t *my_stack, unsigned int *stack_count)
 {
     stack_status_t ret = STACK_NOT_OK;
+
     if (NULL == my_stack || NULL == stack_count)
     {
         ret = STACK_NULL_POINTER;
@@ -156,15 +238,23 @@ stack_status_t stack_count  (stack_ds_t *my_stack, unsigned int *stack_count)
     }
     return ret;
 }
+
+/**
+ * @brief Checks if the stack is full.
+ * 
+ * This function checks if the stack is full.
+ * 
+ * @param my_stack Pointer to the stack structure.
+ * 
+ * @return stack_status_t indicating the status of the operation.
+ */
 stack_status_t stack_full(stack_ds_t *my_stack)
 {
     stack_status_t ret = STACK_NOT_OK;
+
     if (my_stack->stack_count == my_stack->max_size)
     {
         ret = STACK_FULL;
-        #ifdef stack_DEBUG
-        printf("Stack is Full\n");
-        #endif // stack_DEBUG
     }
     else
     {
@@ -172,15 +262,23 @@ stack_status_t stack_full(stack_ds_t *my_stack)
     }
     return ret;
 }
+
+/**
+ * @brief Checks if the stack is empty.
+ * 
+ * This function checks if the stack is empty.
+ * 
+ * @param my_stack Pointer to the stack structure.
+ * 
+ * @return stack_status_t indicating the status of the operation.
+ */
 stack_status_t stack_empty(stack_ds_t *my_stack)
 {
     stack_status_t ret = STACK_NOT_OK;
+
     if (-1 == my_stack->stack_top)
     {
         ret = STACK_EMPTY;
-        #ifdef stack_DEBUG
-        printf("Stack is empty\n");
-        #endif // stack_DEBUG
     }
     else
     {
